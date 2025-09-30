6691dd47026171b32293b38882e802c5aa5c8ede
dillandatanerd <dillanpatelnz@gmail.com>
2025-09-30 18:13:06 +0200

    merge

--- src/pages/TripEditPage.tsx
+++ src/pages/TripEditPage.tsx
@@ -1,6 +1,4 @@
-import { Box, Typography } from "@mui/material";
 import HeaderBar from "../components/HeaderBar";
-import { useParams } from "react-router-dom";
 import TripForm from "../components/TripForm";
 
 function TripEditPage() {
